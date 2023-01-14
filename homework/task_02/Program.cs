cb42adbb812f6cfeb7c0539b48ef21b39f77a3ef
Tikhomirov <tihomir.94@mail.ru>
2023-01-14 03:32:58 +0300

    completed the task04

--- homework/task_02/Program.cs
+++ homework/task_02/Program.cs
@@ -4,7 +4,7 @@
 
 Console.Write("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите первое число: ");
+Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if (num1 > num2)
