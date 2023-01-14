990b23dd7af8d5176bbef480189a06590c8a83d0
Tikhomirov <tihomir.94@mail.ru>
2023-01-14 03:46:06 +0300

    completed the task08

---
+++ homework/task_08/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход
+// принимает число (N), а на выходе показывает все чётные
+// числа от 1 до N.
+
+Console.Write("Введите натуральное число: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+while (userNum <= 0)
+{
+    Console.Write("Вы ввели неверное число! Введите натуральное число: ");
+    userNum = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 1;
+
+while (count <= userNum)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count} ");
+    }
+    count++;
+}
