28fad01a423b2ff2fbfda0ebb684b4a6f397fa5a
Tikhomirov <tihomir.94@mail.ru>
2023-01-17 11:21:27 +0300

    complite task11

---
+++ seminar/task_11/Program.cs
@@ -0,0 +1,22 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+// 12 мин
+
+int rendomNumber = new Random().Next(100, 1000);
+Console.WriteLine($"Конечное число => {rendomNumber}");
+
+int finishNumber = GetNumber(rendomNumber);
+Console.Write($"Конечное число => {finishNumber}");
+
+int GetNumber(int number)
+{
+    int firstNumber = number / 100;
+    int secondNumber = number % 10;
+
+    int result = firstNumber * 10 + secondNumber;
+    return result;
+}
