9cde92f023f37a950640c8147a686326fd19268d
Tikhomirov <tihomir.94@mail.ru>
2023-01-17 12:25:44 +0300

    complite task14 and 16

---
+++ seminar/task_14/Program.cs
@@ -0,0 +1,22 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = GetNumber(number);
+
+int GetNumber(int num)
+{
+    int res1 = num % 7;
+    int res2 = num % 23;
+    if (res1 == 0 && res2 == 0) return 1;
+    else return 0;
+}
+
+if (result == 1) Console.WriteLine("Кратно 7 и 23");
+else Console.WriteLine("Не кратно 7 и 23");
---
+++ seminar/task_16/Program.cs
@@ -0,0 +1,25 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+Console.Write("Введите целое первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите целое второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = GetNumber(firstNumber, secondNumber);
+
+int GetNumber(int num1, int num2)
+{
+    if (num1 * num1 == num2) return 1;
+    else if (num2 * num2 == num1) return 1;
+    else return 0;
+}
+
+if (result == 1) Console.WriteLine("Кратно");
+else Console.WriteLine("Некратно");
