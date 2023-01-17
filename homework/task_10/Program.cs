ff7e727bb64f9e4d400ebaca968d71d071fef727
Tikhomirov <tihomir.94@mail.ru>
2023-01-17 14:28:17 +0300

    complite task10

---
+++ homework/task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+int GetNumber(int num)
+{
+    return num / 10 % 10;
+}
+
+Console.Write("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = GetNumber(number);
+Console.Write($"{number} -> {result}");
