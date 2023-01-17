785d6d50d9afec3c51f344a1732e7ff2a6e6ea78
Tikhomirov <tihomir.94@mail.ru>
2023-01-17 15:07:14 +0300

    complite task15

---
+++ homework/task_15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+bool GetWeekend(int num)
+{
+    return num == 6 || num == 7;
+}
+
+Console.Write("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool res = GetWeekend(number);
+Console.WriteLine(res ? "выходной" : "не выходной");
+
+
+
