e670d55e2dd3c0284b5100d92f3ef9180a24a054
Tikhomirov <tihomir.94@mail.ru>
2023-01-20 16:30:51 +0300

    complite task22

---
+++ seminar/task_22/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+* Написать программу, котрая принимает на вход натуральное число N
+* и выдет таблицу квадратов чисел от 1 до N.
+*/
+
+void GetSquaresTable(int num)
+{
+    int count = 1;
+    Console.Write($"{num} -> ");
+    while (count <= num)
+    {
+        Console.Write($"{count * count} ");
+        count++;
+    }
+
+}
+
+Console.Write("Введите положительное натуральное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+while (userNumber < 1)
+{
+    Console.Write("Вы ввели неправильное число. Введите положительное натуральное число: ");
+    userNumber = Convert.ToInt32(Console.ReadLine());
+}
+
+GetSquaresTable(userNumber);
