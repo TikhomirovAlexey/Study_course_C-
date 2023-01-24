dcb876f53541c44b90584179d4b140c8e087200f
Tikhomirov <tihomir.94@mail.ru>
2023-01-24 15:08:28 +0300

    complite task25

---
+++ homework/task_25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double GetResult(double num, double deg)
+{
+    double res = num;
+    for (int i = 1; i < deg; i++)
+    {
+        res *= num;
+    }
+    return res;
+}
+
+Console.Write("Введите первое число: ");
+double userNumber = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите положительное целое число - степень: ");
+string userDegree = Console.ReadLine();
+
+int numberDegree;
+bool res = int.TryParse(userDegree, out numberDegree);
+
+while (numberDegree <= 0 || !res) /*так как int.TryParse в случае false записывает 0 в указанную переменную, то в условии цикла исключаем отрицательные значения и 0. (res == false или !res можно не вписывать или "дурной тон"?)*/
+{
+    Console.Write("Степень должна быть натуральным числом. Введите положительное целое число: ");
+    userDegree = Console.ReadLine();
+    res = int.TryParse(userDegree, out numberDegree);
+}
+
+double result = GetResult(userNumber, numberDegree);
+Console.WriteLine($"{userNumber} в степени {numberDegree}: {result}.");
